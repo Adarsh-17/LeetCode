4610a1f1882fe4cfd71b3ffadc97ed886cd5d82d
Adarsh Aniket <84972243+Adarsh-17@users.noreply.github.com>
2022-08-04 10:02:59 +0530

    Time: 41 ms (5.56%), Space: 43.6 MB (5.56%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,11 +1,9 @@
 class Solution {
     public int mirrorReflection(int p, int q) {
-        int ext = q;
-        int ref = p;
-        while(ext%2==0 && ref%2==0){
-            ext = ext/2;
-            ref = ref/2;
-        }
+        int ext = lcm(p,q)/p;
+        int ref = lcm(p,q)/q;
+        System.out.println(lcm(p,q));
+        
         if(ext%2==0 && ref%2==1){
             return 0;
         } else if(ext%2==1 && ref%2==0){
@@ -14,5 +12,16 @@ class Solution {
             return 1;
         }
         return 0;
+    }
+    
+    public int lcm(int p,int q){
+        int x = p>q? p : q;
+        int y = p>q? q : p;
+        int count = 1;
+        while(!((x*count)%y==0)){
+            count++;
+            System.out.println(x*count);
+        }
+        return x*count;
     }
 }
