3643846ac43e9df7cf0ab15f26b625a4ac921aa7
Adarsh Aniket <84972243+Adarsh-17@users.noreply.github.com>
2022-08-11 19:44:57 +0530

    Time: 0 ms (100.00%), Space: 42 MB (67.31%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,18 +15,36 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-         List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> ans = new ArrayList<>();
         TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
+        while(curr!=null){
+            TreeNode leftNode = curr.left;
+            if(leftNode==null){
+                ans.add(curr.val);
+                curr = curr.right;
+            } 
+            else
+            {
+                TreeNode rightMostNode = getRightMostNode(leftNode,curr);
+                if(rightMostNode.right!=null){
+                     rightMostNode.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+                else {
+                    rightMostNode.right = curr;
+                    curr = curr.left;
+                }
+                    
             }
-            curr = stack.pop();
-            res.add(curr.val);
-            curr = curr.right;
         }
-        return res;
+        return ans;
+    }
+    
+    public TreeNode getRightMostNode(TreeNode leftNode,TreeNode curr){
+        while(leftNode.right!=null && leftNode.right!=curr){
+            leftNode = leftNode.right;
+        }
+        return leftNode;
     }
 }
