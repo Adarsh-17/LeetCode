997a52666f95c2be1d3010a89bbd5762892f25cf
Adarsh Aniket <84972243+Adarsh-17@users.noreply.github.com>
2022-08-31 19:16:12 +0530

    Time: 1 ms (89.89%), Space: 43.7 MB (25.37%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,15 +1,22 @@
 class Solution {
+    List<List<Integer>> output = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-       List<List<Integer>> ans = new ArrayList<>();
-       ans.add(new ArrayList<Integer>());
-       for(int i=0;i<nums.length;i++){
-           int len = ans.size();
-           for(int j=0;j<len;j++){
-               List<Integer> op = new ArrayList<>(ans.get(j));
-               op.add(nums[i]);
-               ans.add(op);
-           }
-       }
-       return ans;
+        subsetsHelp(nums,0);
+        return output;
+    }
+    
+    public void subsetsHelp(int[] nums,int index){
+        if(index==nums.length){
+            output.add(new ArrayList<Integer>());
+            return;
+        }
+        subsetsHelp(nums,index+1);
+        int len = output.size();
+        for(int i=0;i<len;i++){
+            List<Integer> ans = new ArrayList<>(output.get(i));
+            ans.add(nums[index]);
+            output.add(new ArrayList<>(ans));
+        }
+        return;
     }
 }
