3b6f26c22fb48af13cc2e18a82bae8b905f351bb
Adarsh Aniket <84972243+Adarsh-17@users.noreply.github.com>
2022-10-28 10:11:44 +0530

    Time: 41 ms (68.29%), Space: 43.7 MB (39.02%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,23 +1,42 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
-        int maxcount = 0;
+        int maxCount = 0;
         for(int i=0;i<img1.length;i++){
-            for(int j=0;j<img2.length;j++){    
-               maxcount = Math.max(maxcount,countOverlap(img1,img2,i,j));
-               
-               maxcount = Math.max(maxcount,countOverlap(img2,img1,i,j));
+            for(int j=0;j<img2.length;j++){
+                maxCount = Math.max(maxCount,shiftAndCount(img1,img2,i,j));
+                maxCount = Math.max(maxCount,shiftAndCount(img2,img1,i,j));
             }
         }
-        return maxcount;
+        return maxCount;
     }
     
-    
-    public int countOverlap(int[][] M,int[][] R,int x,int y){
-                int leftShiftCount = 0, rightShiftCount = 0;
+    public int getMatchCount(int[][] A,int[][] B,int startAI,int startAJ){
+        int leftcount = 0;
+        int rightcount = 0;
+        int startBI = 0;
+        int startBJ = 0;
+        for(int i=startAI;i<A.length;i++){
+            startBJ = 0;
+            for(int j=startAJ;j<B.length;j++){
+                if(A[i][j]==1&&A[i][j]==B[startBI][startBJ]){
+                    leftcount++;
+                }
+                if(B[i][j]==1&&B[i][j]==A[startBI][startBJ]){
+                    rightcount++;
+                }
+                startBJ++;
+            }
+            startBI++;
+        }
+        return Math.max(rightcount,leftcount);
+    }
+    protected int shiftAndCount(int[][] M, int[][] R,int xShift,int yShift) {
+        int leftShiftCount = 0, rightShiftCount = 0;
         int rRow = 0;
-        for (int mRow = y; mRow < M.length; mRow++) {
+        // count the cells of ones in the overlapping zone.
+        for (int mRow = yShift; mRow < M.length; ++mRow) {
             int rCol = 0;
-            for (int mCol = x; mCol < M.length; mCol++) {
+            for (int mCol = xShift; mCol < M.length; ++mCol) {
                 if (M[mRow][mCol] == 1 && M[mRow][mCol] == R[rRow][rCol])
                     leftShiftCount += 1;
                 if (M[mRow][rCol] == 1 && M[mRow][rCol] == R[rRow][mCol])
