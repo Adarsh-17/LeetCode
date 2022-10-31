13e8206711fad57c431c7422f5ab3ff12af10729
Adarsh Aniket <84972243+Adarsh-17@users.noreply.github.com>
2022-10-31 15:50:09 +0530

    Time: 2 ms (42.33%), Space: 45.5 MB (68.98%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        for(int i=0;i<matrix[0].length;i++){
-           for(int j=0;j<matrix.length;j++){
-              int start = matrix[j][i];
-              int diag_x = i+1;
-              int diag_y = j+1;
-              while(diag_x<matrix[0].length && diag_y<matrix.length){
-                if(matrix[diag_y][diag_x]!=start){
-                    System.out.println(diag_y+"->"+diag_x);
+         for (int row = 0; row < matrix.length; row++){
+            for (int col = 0; col < matrix[0].length; col++){
+                if (row > 0 && col > 0 && matrix[row-1][col-1] != matrix[row][col]){
                     return false;
                 }
-                diag_y++;
-                diag_x++;
-                }
-           }
-            
-        }
+            }
+         }
         return true;
     }
 }
