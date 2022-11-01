5feddb1efad16f44a7244d0f612e7c2cddd3be17
Adarsh Aniket <84972243+Adarsh-17@users.noreply.github.com>
2022-11-01 18:01:15 +0530

    Time: 1 ms (96.80%), Space: 54.5 MB (24.90%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -8,13 +8,16 @@ class Solution {
     }
     
     public int fallsAtBottom(int[][] grid,int row,int col){
-        if(row==grid.length){
-            return col;
-        }
-        int nextCol =  col + grid[row][col];
-         if (nextCol < 0 || nextCol > grid[0].length - 1 || grid[row][col] != grid[row][nextCol])         {
+        while(row<grid.length){
+         int nextCol =  col + grid[row][col];
+         if (nextCol < 0 || nextCol > grid[0].length - 1 || grid[row][col] != grid[row][nextCol])                     {
             return -1;
+          } 
+          row++;
+          col = nextCol;
+         
         }
-        return fallsAtBottom(grid,row+1,nextCol);
+       
+        return col;
     }
 }
